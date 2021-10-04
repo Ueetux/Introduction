3320399e9bf0bf6c1027767247035e7e70f405ad
Hector <hectorlol2426@gmail.com>
2021-10-04 12:17:59 -0700

    implimented asking one question and getting answer

---
+++ hsu_hector_p1q1.py
@@ -0,0 +1,30 @@
+import time
+from rich import print
+
+# Quiz has 5 questions the user will answer.
+# It will keep track of score and give
+# a final result.
+
+# Introduction
+print("Welcome to QwiZ.")
+time.sleep(2)
+print("Good luck!")
+
+# Create a list of questions
+questions = [
+    ["What is the colour of the [bold italic red]sun[/bold italic red]? ", "yellow"]
+]
+
+# For each question, print it out and ask the user to answer
+for question in questions:
+    # Print the question
+    print(question[0])
+
+    # Get the user's answer
+    user_answer = input().lower().strip(",..?!")
+
+    # See if they're correct
+    if user_answer == question[1]:
+        print("dame u got it right! good job!")
+    else:
+        print("Yikes u fail bye bye!")
